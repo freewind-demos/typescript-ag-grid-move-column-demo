97af1f157fd7dfbe1aaa8610d56d874f71b9ce9c
freewind <nowindlee@gmail.com>
2019-04-18 19:55:36 +0800

    demo

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-TypeScript AgGrid Demo
-======================
+TypeScript AgGrid Move Column Demo
+=================================
 
-纯DOM的AgGrid.
+可以移动表格的列，改变它们的顺序，同时在`onColumnMoved`拿到新的column state
 
 ```
 npm install
--- hello.ts
+++ hello.ts
@@ -1,38 +1,23 @@
-import {Grid, GridOptions} from "ag-grid-community";
+import {Grid, ColumnMovedEvent} from "ag-grid-community";
 
 import "ag-grid-community/dist/styles/ag-grid.css";
 import "ag-grid-community/dist/styles/ag-theme-balham.css";
 
-class SimpleGrid {
-  private gridOptions: GridOptions = <GridOptions>{};
+const eGridDiv: HTMLElement = <HTMLElement>document.querySelector('#myGrid');
 
-  constructor() {
-    this.gridOptions = {
-      columnDefs: this.createColumnDefs(),
-      rowData: this.createRowData()
-    };
-
-    let eGridDiv:HTMLElement = <HTMLElement>document.querySelector('#myGrid');
-    new Grid(eGridDiv, this.gridOptions);
+new Grid(eGridDiv, {
+  columnDefs: [
+    {headerName: "Make", field: "make"},
+    {headerName: "Model", field: "model"},
+    {headerName: "Price", field: "price"}
+  ],
+  rowData: [
+    {make: "Toyota", model: "Celica", price: 35000},
+    {make: "Ford", model: "Mondeo", price: 32000},
+    {make: "Porsche", model: "Boxter", price: 72000}
+  ],
+  onColumnMoved: (event: ColumnMovedEvent): void => {
+    const state = event.columnApi.getColumnState()
+    console.log("new state: ", state)
   }
-
-  // specify the columns
-  private createColumnDefs() {
-    return [
-      {headerName: "Make", field: "make"},
-      {headerName: "Model", field: "model"},
-      {headerName: "Price", field: "price"}
-    ];
-  }
-
-  // specify the data
-  private createRowData() {
-    return [
-      {make: "Toyota", model: "Celica", price: 35000},
-      {make: "Ford", model: "Mondeo", price: 32000},
-      {make: "Porsche", model: "Boxter", price: 72000}
-    ];
-  }
-}
-
-new SimpleGrid();
+});
